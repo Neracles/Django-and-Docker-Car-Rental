04775cc9988511a51c74fe3b42d651ca29afdfd5
Daniel Alvestad <danielalvestad6@gmail.com>
2022-10-11 12:57:40 +0200

    Updated database in settings.py. We should set up a MySQL database using these settings at some point.

--- settings.py
+++ settings.py
@@ -75,12 +75,16 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'djangodb',  
+        'USER':'root',  
+        'PASSWORD':'mysql',  
+        'HOST':'localhost',  
+        'PORT':'3306'  
+    }  
+}  
 
 
 # Password validation
