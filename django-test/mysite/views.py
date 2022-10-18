9190c6f8b4533b1457b11f9007c7ab2d7b38e2a3
Anita Vuong Nguyen <anit@live.no>
2022-10-18 11:55:36 +0200

    Lagt til admin.py og views.py under mysite

---
+++ django-test/mysite/views.py
@@ -0,0 +1,42 @@
+from .models import Car
+from rest_framework.response import Response
+from .serializers import CarSerializer
+from rest_framework import status
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def get_cars(request):
+    cars = Car.objects.all()
+    serializer = CarSerializer(cars, many=True)
+    print(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def save_car(request):
+    serializer = CarSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['PUT'])
+def update_car(request, id):
+    try:
+        theCar = Car.objects.get(pk=id)
+    except Car.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)    
+    serializer = CarSerializer(theCar, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_car(request, id):
+    try:
+        theCar = Car.objects.get(pk=id)
+    except Car.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)    
+    theCar.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
