5bb99f29f64e7f81b5a6a69b5bed9c2af1a70fe5
Daniel Alvestad <danielalvestad6@gmail.com>
2022-10-25 10:21:25 +0200

    Added Employee and Customer serializers in serializers file.

--- django-test/mysite/mysite/serializers.py
+++ django-test/mysite/mysite/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
 from models import Car
 from models import Employee
+from models import Customer
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = ['id', 'make', 'carmodel']
+        fields = ['id', 'make', 'carmodel', 'year', 'location', 'status']
         
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = ['id', 'make', 'carmodel'] 
+        fields = ['id', 'name', 'age', 'address', 'branch'] 
+        
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        field = ['name', 'age', 'address', 'cars']
