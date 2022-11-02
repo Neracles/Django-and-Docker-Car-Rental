7603c6446f0cba8258acd25f9d287d7f17778909
smudoperator <simon.halland99@gmail.com>
2022-11-02 12:52:04 +0100

    added cancel_order_car method

--- django-test/mysite/mysite/Views/carViews.py
+++ django-test/mysite/mysite/Views/carViews.py
@@ -1,4 +1,5 @@
 from ..models import Car
+from ..models import Customer
 from rest_framework.response import Response
 from ..serializers import CarSerializer
 from rest_framework import status
@@ -55,6 +56,20 @@ def order_car(request, id):
     if theCar.car_status == "available":
         serializer = CarSerializer(theCar, data=request.data)
         serializer.update(theCar, {"car_status": "booked"})
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)  
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET','PUT'])
+def cancel_order_car(request, carid, custid):
+    try:
+        theCar = Car.objects.get(pk=carid)
+        theCustomer = Customer.objects.get(pk=custid)
+    except Car.DoesNotExist or Customer.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND) 
+    if theCustomer.order == carid:
+        serializer = CarSerializer(theCar, data=request.data)
+        serializer.update(theCar, {"car_status": "available"})
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)  
     return Response(status=status.HTTP_204_NO_CONTENT)
